66d79eb187a41366afd702d1f3d9a092337a73a4
Agneta <agnetagsu@yahoo.es>
2021-05-22 19:55:04 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Flujo_de_datos___C2_Ej3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad");
+            string edad = Console.ReadLine();
+            Console.WriteLine("Te llamas " + nombre + " y tienes " + edad + " años");
+        }
+    }
+}
